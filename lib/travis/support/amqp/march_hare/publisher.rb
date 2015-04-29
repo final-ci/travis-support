e496e14fb6bba39a0c1a8dcee9b935ce9e7de481
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-04-29 11:22:53 +0200

    feature: improve Amqp (Bunny) wrapper
    support for travis-worker to be able to use march_hare as well as bunny.
    e.g. use MRI as well as jruby

--- lib/travis/support/amqp/march_hare/publisher.rb
+++ lib/travis/support/amqp/march_hare/publisher.rb
@@ -16,14 +16,24 @@ module Travis
         @options = options.dup
         @name = @options.delete(:name) || ""
         @type = @options.delete(:type) || "direct"
+        @shared_channel = @options.delete :unique_channel
+        @dont_retry = @options.delete :dont_retry
       end
 
       def publish(data, options = {})
         data = MultiJson.encode(data)
         defaults = { :routing_key => routing_key, :properties => { :message_id => rand(100000000000).to_s } }
-        retrying do
+        if @dont_retry
           exchange.publish(data, deep_merge(defaults, options))
+        else
+          retrying do
+            exchange.publish(data, deep_merge(defaults, options))
+          end
         end
+      end
+
+      def channel
+        @channel ||= @unique_channel ? Amqp.connection.create_channel : self.class.channel
       end
 
       protected
