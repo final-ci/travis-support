19393c31c291b045a83ddb0a68818e68819ffc93
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-11-24 13:17:12 +0100

    Add test results database

--- lib/travis/support/database.rb
+++ lib/travis/support/database.rb
@@ -15,7 +15,8 @@ module Travis
 
         ActiveRecord::Base.configurations = {
           Travis.env => Travis.config.database,
-          'logs_database' => Travis.config.logs_database || Travis.config.database
+          'logs_database' => Travis.config.logs_database || Travis.config.database,
+          'test_results_database' => Travis.config.test_results_database || Travis.config.database
         }
 
         ActiveRecord::Base.establish_connection(Travis.env)
